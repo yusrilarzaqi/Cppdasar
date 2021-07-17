63a0366a3222b49e6ba45e15b807ece1565f8a49
yusrilarzaqi <arzaqiyusril@gmail.com>
2021-07-18 01:51:31 +0700

    edit 09

--- 09.tipe_data/tipe.cpp
+++ 09.tipe_data/tipe.cpp
@@ -3,10 +3,24 @@ using namespace std;
 
 int main(){
    // bilangan bulat
-   int a = 10;
+   // int a = 10;// 32-bit
+   // long a =999; 64-bit
+   // short a = 2;
+   
+   // bilangan decimal
+   // float a = 1.0; 32-bit
+   // double a = 20.2 64-bit
 
+
+    //character
+    char a = 'b';
 
    cout << a << endl;
-   cout << numeric_limits<intinintt>
-inintt}
+   cout << sizeof(a) << "byte" << endl;
+   cout << numeric_limits<char>::max() << endl;
+   cout << numeric_limits<char>::min() << endl;
+   cin.get();
+   return 0;
+  
+}
 
