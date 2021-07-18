3515c076d6fffc2e868e84a8b1b414c8e01a920a
yusrilarzaqi <arzaqiyusril@gmail.com>
2021-07-18 12:14:34 +0700

    add 10,12,13

---
+++ 13.if/if.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "masukan angka : ";
+    cin >> a;
+    
+    // if statement
+    // kondisi dalam bentuk boolean true
+    if(a == 3)
+    {
+        cout << "benar" << endl;
+    }
+
+    cout << "selesai" << endl;
+    return 0;
+}
+
