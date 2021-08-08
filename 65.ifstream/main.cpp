011e99efde5bc11df0feaa8cd8c74990056ba3e3
yusrilarzaqi <arzaqiyusril@gmail.com>
2021-08-08 20:04:59 +0700

    modified 65

--- 65.ifstream/main.cpp
+++ 65.ifstream/main.cpp
@@ -6,16 +6,30 @@ using namespace std;
 int main(){
 
     ifstream myFile;
-    string data, buffer;
-    int jumlah;
-    bool ulang = false;
+    string buffer, nama, output;
+    int jumlah=0;
+    int no;
+    bool ulang = true;
 
-    myFile.open("data.txt");
+    myFile.open("data.txt", ios::in);
+    while(ulang){
+        getline(myFile, buffer);
+        output.append("\n"+buffer);
+        if(buffer == "daftar")break;
+    }
+    // myFile.close();
+    cout << output << endl;
 
-    myFile.close();
-
+    getline(myFile, buffer);
+    cout << buffer << endl;
+    while(!myFile.eof()){
+        myFile >> no;
+        myFile >> nama;
+        
+        jumlah++;
+        cout <<  no << "   " << nama << endl;
+    }
+    cout << "jumlah : " << jumlah << endl;
 
     return 0;
 }
-
-
