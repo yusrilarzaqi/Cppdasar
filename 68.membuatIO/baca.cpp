9813254477dc4722bb4187f284e9bb26fceb732d
yusrilarzaqi <yusrilarzaqi@gmail.com>
2021-08-15 21:39:08 +0700

    modified 68.membuatIO/baca.cpp

--- 68.membuatIO/baca.cpp
+++ 68.membuatIO/baca.cpp
@@ -9,26 +9,25 @@ struct Mahasiswa{
     string jurusan;
 };
 
+Mahasiswa ambilData(int &posisi, fstream &myFile){
+    Mahasiswa bufferData;
+    myFile.seekp((posisi -1)* sizeof(Mahasiswa));
+    myFile.read(reinterpret_cast<char*>(&bufferData), sizeof(Mahasiswa));
+    return bufferData;
+}
+
 int main(){
     fstream myFile;
     Mahasiswa dataBaca;
     myFile.open("data.bin", ios::in | ios::binary);
-    
-    myFile.read(reinterpret_cast<char*>(&dataBaca), sizeof(Mahasiswa));
+
+    int posisi = 2;
+    dataBaca = ambilData(posisi, myFile);
+
     cout << dataBaca.NIM  << endl;
     cout << dataBaca.nama  << endl;
     cout << dataBaca.jurusan  << endl << endl;
      
-    myFile.read(reinterpret_cast<char*>(&dataBaca), sizeof(Mahasiswa));
-    cout << dataBaca.NIM  << endl;
-    cout << dataBaca.nama  << endl;
-    cout << dataBaca.jurusan  << endl << endl;
-   
-    myFile.read(reinterpret_cast<char*>(&dataBaca), sizeof(Mahasiswa));
-    cout << dataBaca.NIM  << endl;
-    cout << dataBaca.nama  << endl;
-    cout << dataBaca.jurusan  << endl << endl;
-
     myFile.close();
 
     return 0;
